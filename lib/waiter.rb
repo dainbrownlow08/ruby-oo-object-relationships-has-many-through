2c58e8f4983eace0316f2f2731934585de09f0e9
dainbrownlow08 <dain.brownlow@outlook.com>
2020-11-19 15:18:11 -0800

    commit tree 7698d033dd5d37cbe2c3aa4d6ab91c0114709f06

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
+    attr_reader :name
+    
+    @@all
+    def initialize(name,exp)
+        @name = name
+        @exp = exp
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer,total,tip)
+        Meal.new(self,customer,total,tip)
+    end
+
+    def meals
+        Meal.all.select{|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        meals.max_by{|meal| meal.tip}.customer
+    end
+
 
 end
